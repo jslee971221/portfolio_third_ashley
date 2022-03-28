d349bceb959045a5e7ab8502c1aab9efa5690579
jslee971221 <88294804+jslee971221@users.noreply.github.com>
2022-03-28 17:39:16 +0900

    index 페이지 태블릿, 모바일 작업

--- js/index.js
+++ js/index.js
@@ -4,9 +4,56 @@ $(function(){
     var c2Thumb = $('.cnt02 .artGroup .artThumb');
     var c2Img = $('.cnt02 .artGroup .image');
     var c2btn = $('.cnt02 .artGroup .btn div');
+    var pBtn = $('.cnt01 .paging li');
+    var c1Img = $('.cnt01 .section .image');
+    var c1Article = $('.cnt01 .artRight .image')
+    var c1ArtView = $('.cnt01 .artLeft .image')
+    var c1ArtViewImg = c1ArtView.css('backgroundImage');
     var imgs = [];
     var txt = [];
+    var changeImg = function(imgs, txts, thumbImg, thumbTxt, ci){
+        for(i = 0; i < 7; i++){
+            imgs[i] = thumbImg.eq(i).css('backgroundImage'); 
+            txts[i] = thumbTxt.eq(i).html();
+        }
+        for(var i = 0; i < 7; i++){
+            if(ci > 6){
+                ci = 0;
+            }
+            thumbImg.eq(i).css('backgroundImage', imgs[ci]);
+            thumbTxt.eq(i).html(txts[ci]);
+            ci++;
+        }     
+    }
     
+    c1Img.click(function(){
+        var ind = $(this).index() + 1;
+        var c1Imgs = [];
+        for(i = 0; i < 4; i++){
+            c1Imgs[i] = c1Img.eq(i).css('backgroundImage'); 
+        }
+        c1ArtView.css('backgroundImage', c1Imgs[ind]);
+        for(i = 0; i < 3; i++){
+            ind++;
+            if(ind > 3){
+                ind = 0;
+            }
+            c1Article.eq(i).css('backgroundImage', c1Imgs[ind]);
+        }
+    });
+    pBtn.click(function(){
+        var i = $(this).index();
+        if(i == 0){
+            c1ArtView.css('backgroundImage', c1ArtViewImg);
+        }else{
+            c1ArtView.css('backgroundImage', c1Article.eq(i - 1).css('backgroundImage'));
+        }
+        pBtn.removeClass('show');
+        $(this).addClass('show');
+    });
+
+
+
     c2Thumb.click(function(){
         c2i = $(this).index();
         for(var i = 0; i <= 6; i++){
@@ -25,37 +72,12 @@ $(function(){
     });
 
     c2btn.eq(1).click(function(){
-        c2i = 0;
-        for(i = 0; i < 7; i++){ //현재 이미지 상태 저장
-            imgs[i] = c2Img.eq(i).css('backgroundImage');
-            txt[i] = c2txt.eq(i).html();
-        }
-        for(var i = 0; i < 7; i++){
-            c2i++;
-            if(c2i > 6){
-                c2i = 0;
-            }
-            c2Img.eq(i).css('backgroundImage', imgs[c2i]); //이미지 변경
-            c2txt.eq(i).html(txt[c2i]);
-        }          
+        c2i = 1;
+        changeImg(imgs, txt, c2Img, c2txt, c2i);       
     });
     c2btn.eq(0).click(function(){
         c2i=6;
-        if(c2i < 0){
-            c2i = 6;
-        }
-        for(i = 0; i < 7; i++){
-            imgs[i] = c2Img.eq(i).css('backgroundImage'); //현재 태저장
-            txt[i] = c2txt.eq(i).html();
-        }
-        for(var i = 0; i < 7; i++){
-            if(c2i > 6){
-                c2i = 0;
-            }
-            c2Img.eq(i).css('backgroundImage', imgs[c2i]);
-            c2txt.eq(i).html(txt[c2i]);
-            c2i++;
-        }     
+        changeImg(imgs, txt, c2Img, c2txt, c2i);
     });
     
 });
