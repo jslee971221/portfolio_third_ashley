96b55b4f51d5137099712cba1868064d8c56052e
jslee971221 <88294804+jslee971221@users.noreply.github.com>
2022-03-29 17:38:21 +0900

    index 마무리, sub1 페이지 pc 작업, 태블릿 시작, 폰트 적용

---
+++ js/sub1.js
@@ -0,0 +1,54 @@
+$(function(){
+    var thumb = $('.artBtm .article');
+    var view = $('.artMid .article')
+    var likes = $('.article .btn');
+    var more = $('.section>.btn');
+    var i = 0;
+    var counter = 1;
+    var likec = 1;
+    
+    thumb.click(function(){
+        var th = $(this);
+        i = th.index();
+        if(th.parent().attr('class') == 'artGroup g2'){
+            i += 5;
+        }
+        view.html(th.html());
+        view.find('.image').css('backgroundImage', th.find('.image').css('backgroundImage'));
+    });
+
+    view.on('click', '.btn', function(){
+        if(likec == -1){
+            var icon = $(this).find('i');
+            if(icon.attr('class') == 'xi-heart-o'){
+                likec = 1;
+            }
+        }
+
+        if(likec == 1){
+            view.find('i').removeClass('xi-heart-o');
+            view.find('i').addClass('xi-heart');
+            var count = Number($('.texts span').eq(0).text());
+            $('.texts span').eq(0).text(count + 1);
+        }else{
+            view.find('i').removeClass('xi-heart');
+            view.find('i').addClass('xi-heart-o');
+            var count = Number($('.texts span').eq(0).text());
+            $('.texts span').eq(0).text(count - 1);
+        }
+        likec *= -1;
+        thumb.eq(i).find('.texts').html(view.find('.texts').html());
+        thumb.eq(i).find('.image .btn').html(view.find('.image .btn').html());
+    });
+
+    more.click(function(){
+        if(counter == 1){
+            $('.artBtm').append($('.artBtm').html());
+            $(this).text('Close -');
+        }else{
+            $('.artBtm .artGroup:eq(1)~').remove();
+            $(this).text('View More +');
+        }
+        counter *= -1;
+    });
+});
